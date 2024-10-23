5f8a9e893b6701eae4cf2a22f88f244b7d16b599
Andyzhongzwk <131367457+Andyzhongzwk@users.noreply.github.com>
2024-10-23 19:49:09 +0800

    Update index.js
    Finished all scraping and cleanning funcitons, while some (<10%) special cases need to be handled manually.

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-function Program(code, name_short, name, name_chi, uni, required_elective, jupaslink) {
+// Constructor for a Program object
+function Program(code, name_short, name, name_chi, uni, jupaslink, required_elective1, required_elective2, required_elective3, preferred_electives) {
     this.code = code;
     this.name_short = name_short;
     this.name = name;
     this.name_chi = name_chi;
     this.uni = uni;
-    this.required_elective = required_elective;
     this.jupaslink = jupaslink;
+    this.required_elective1 = required_elective1;
+    this.required_elective2 = required_elective2;
+    this.required_elective3 = required_elective3;
+    this.preferred_electives = preferred_electives;
 }
 
+// Helper function to flatten an array
+function flattenArray(arr) {
+    let flattenedArray = [];
+
+    arr.forEach(element => {
+        if (Array.isArray(element)) {
+            flattenedArray = flattenedArray.concat(flattenArray(element));
+        } else {
+            flattenedArray.push(element);
+        }
+    });
+
+    return flattenedArray;
+}
+    
+// Helper function to capitalize the first word of a string
 function capitalizeFirstWord(sentense) {
     const words = sentense.split(" ");
 
     for (let i = 0; i < words.length; i++) {
-        if (words[i] == "AND") {
+        if (words[i] == "AND" || words[i] == "OR") {
             words[i] = words[i].toLowerCase();
-        } else if (words[i][0] == "(") {
-            words[i] = words[i].toUpperCase();
         } else {
         words[i] = words[i][0].toUpperCase() + words[i].substring(1).toLowerCase();
         }
@@ -27,10 +45,23 @@ function capitalizeFirstWord(sentense) {
     return words.join(" ");
 }
 
+// Helper function to double quoting data
 function quoteData(data) {
-    return `"${data.replace(/"/g, '""')}"`; // Double quote the data before exporting to CSV file
+    return `"${data.replace(/"/g, '""')}"`; 
 }
 
+// Helper function to get the link of schools
+function getSchoolLinks() {
+    const schoolnames = ["cityuhk", "hkbu", "lingnanu", "cuhk", "eduhk", "polyu", "hkust", "hku", "hkmu"];
+    const schoolLinks = [];
+    for (let i = 0; i < schoolnames.length; i++) {
+        let link = `https://www.jupas.edu.hk/tc/programme/${schoolnames[i]}/`;
+        schoolLinks.push(link);
+    }
+    return schoolLinks;
+}
+
+// Get a list of required electives from an url
 async function getElectives(page, url) {
     await page.goto(url);
 
@@ -41,12 +72,16 @@ async function getElectives(page, url) {
         }
 
         const rows = table.querySelectorAll('tbody tr');
-        const data = new Set();
+        const data = new Set(); // To prevent duplicate value
 
         rows.forEach(row => {
             const elective = row.querySelector('.dsereg-sub').innerText.trim();
-            if (elective !== 'Or' && !elective.includes('ANY') && !elective.includes('of') && !elective.includes('EXTENDED')) {
-                data.add(elective);
+            if (elective !== 'Or' && !elective.includes('ANY') && !elective.includes('of')) { // Exclude "ANY 1 SUBJECT" and "One/Two of the following elective subjects:" 
+                let cleaned_data = elective.replace(/note\w{0,1}/gi, ''); // Exclude footers "note1" or "note2"
+                cleaned_data = cleaned_data.split("or"); // Split the case of "BIOLOGY or CHEMISTRY or PHYSICS or INFORMATION AND COMMUNICATION TECHNOLOGY"
+                for (const d of cleaned_data) {
+                    data.add(d.trim());
+                }
             }
         });
 
@@ -60,17 +95,7 @@ async function getElectives(page, url) {
     return tableData;
 }
 
-
-function getSchoolLinks() {
-    const schoolnames = ["cityuhk", "hkbu", "lingnanu", "cuhk", "eduhk", "polyu", "hkust", "hku", "hkmu"];
-    const schoolLinks = [];
-    for (let i = 0; i < schoolnames.length; i++) {
-        let link = `https://www.jupas.edu.hk/tc/programme/${schoolnames[i]}/`;
-        schoolLinks.push(link);
-    }
-    return schoolLinks;
-}
-
+// Check and return the number of available programs in a program page
 async function checkAvailableProgram(page, url) {
     let num = 0;
     try {
@@ -89,19 +114,62 @@ async function checkAvailableProgram(page, url) {
     }
 }
 
+// Helper function to rename subjects format, also double quote the subjects, a list is returned
+function renameElective(elective) {
+    const renamed = [];
+
+    const subject_replacements = {
+        "Information and Communication Technology": "Information and Communication Technology (ICT)",
+        "Mathematics Extended Module 1 or 2": [
+            "Mathematics Extended Part Module 1 (M1)",
+            "Mathematics Extended Part Module 2 (M2)"
+        ],
+        "Mathematics Compulsory Part": "",
+        "Literature in English": "English Literature",
+        "BAFS (accounting / Business Management)": "Business, Accounting and Financial Studies (BAFS)",
+        "Accounting and Financial Studies (accounting)": "Business, Accounting and Financial Studies (BAFS)",
+        "Accounting and Financial Studies (business Management)": "Business, Accounting and Financial Studies (BAFS)",
+        "Business": "",
+        "Combined Science": "Science: Combined Science",
+        "Integrated Science": "Science: Integrated Science",
+        "Technology and Living (food Science and Technology)": "Technology and Living",
+        "Technology and Living (fashion, Clothing and Textiles)": "Technology and Living"
+    };
+
+    // Check if the elective exists in the replacements, also double quote the subjects
+    if (elective in subject_replacements && subject_replacements[elective].length == 2) {
+        renamed.push(quoteData("Mathematics Extended Part Module 1 (M1)"));
+        renamed.push(quoteData("Mathematics Extended Part Module 2 (M2)"));
+    } else if (elective in subject_replacements) {
+        renamed.push(quoteData(subject_replacements[elective]));
+    } else {
+        renamed.push(quoteData(elective));
+    }
+    
+    return renamed; // A list is returned
+}
+
+// Function to get all program detail information, a list of Program objects is returned
 async function getProgramDetail(page) {
-    const programs = []; // List of Program objects
+    const programs = []; // List of Program objects to be returned
+
     const schoolLinks = getSchoolLinks();
     const schoolnames = ["cityuhk", "hkbu", "lingnanu", "cuhk", "eduhk", "polyu", "hkust", "hku", "hkmu"];
     const schoolNames = ["CityU", "HKBU", "LU", "CUHK", "EdUHK", "PolyU", "HKUST", "HKU", "MU"];
 
-    for (let i = 0; i < schoolLinks.length; i++) {
+    // const schoolLinks = ["https://www.jupas.edu.hk/tc/programme/hku/"];
+    // const schoolnames = ["hku"];
+    // const schoolNames = ["HKU"];
+
+    for (let i = 0; i < schoolLinks.length; i++) { // Loop through all school links
         try {
+            // 1.Check and get the number of available program
             const numOfProgram = await checkAvailableProgram(page, schoolLinks[i]);
             let index = 1;
             while (index < numOfProgram + 1) {
                 await page.goto(schoolLinks[i]);
 
+                // 2. Get program information
                 const selector_code = `#main > div.container > article > div.pageContent > div.program_list > table > tbody > tr:nth-child(${index}) > td.c-no > a`;
                 const selector_shortname = `#main > div.container > article > div.pageContent > div.program_list > table > tbody > tr:nth-child(${index}) > td.c-sn`;
                 const selector_engname = `#main > div.container > article > div.pageContent > div.program_list > table > tbody > tr:nth-child(${index}) > td.c-ft`;
@@ -116,25 +184,43 @@ async function getProgramDetail(page) {
                     return { code, shortname, engname, chiname };
                 }, selector_code, selector_shortname, selector_engname, selector_chiname);
 
+                // 3. Get program required electives information
                 const uni = schoolNames[i];
                 const url = `https://www.jupas.edu.hk/tc/programme/${schoolnames[i]}/${programData.code}/`;
                 const electives = await getElectives(page, url);
-                for (let j = 0; j < electives.length; j++) {
-                    electives[j] = capitalizeFirstWord(electives[j]);
+
+                // 4. Clean the data
+                let cleaned_electives_set = new Set();
+                let cleaned_electives_list = [];
+                if (!(electives.length == 1 && electives[0].includes("EXTENDED MODULE"))) {
+                    for (let j = 0; j < electives.length; j++) {
+            
+                        electives[j] = capitalizeFirstWord(electives[j]);
+                        const bufferlist = renameElective(electives[j]);
+                        for (const buffer of bufferlist) {
+                            cleaned_electives_set.add(buffer);
+                        }
+            
+                    }
+                    cleaned_electives_list = flattenArray(Array.from(cleaned_electives_set));
                 }
 
+                // 5. Store as a program object
                 const program = new Program(
                     programData.code, 
                     programData.shortname, 
                     programData.engname, 
                     programData.chiname, 
                     uni, 
-                    electives, 
-                    url);
+                    url,
+                    cleaned_electives_list,
+                    [],
+                    [],
+                    []);
 
-                console.log(program);
                 programs.push(program);
 
+                // 6. Proceed to next program
                 index++;
             }
         } catch (error) {
@@ -144,25 +230,41 @@ async function getProgramDetail(page) {
     return programs;
 }
 
+// Helper function to export the program list to CSV file
 function exportToCSV(programs) {
-    const header = ['Code', 'Short Name', 'English Name', 'Chinese Name', 'University', 'Required Electives', 'JUPAS Link'];
+    const header = ['Code',
+        'Short Name',
+        'English Name',
+        'Chinese Name',
+        'University',
+        'JUPAS Link',
+        'Required Electives1',
+        'Required Electives2',
+        'Required Electives3',
+        'Preferred Electives'];
+
+    // Double quote some data so that they can be seperated properly
     const rows = programs.map(program => [
         program.code,
-        program.name_short,
+        quoteData(program.name_short),
         quoteData(program.name),
         quoteData(program.name_chi),
         program.uni,
-        program.required_elective.join('; '), // Joining electives with semicolons
-        program.jupaslink
+        program.jupaslink,
+        quoteData(program.required_elective1.join(', ')),
+        [],
+        [],
+        []
     ]);
 
     const csvContent = [header, ...rows].map(e => e.join(",")).join("\n");
 
     // Write to a CSV file with UTF-8 encoding
-    fs.writeFileSync('programs.csv', "\uFEFF" + csvContent, 'utf8');
-    console.log('CSV file has been saved as programs.csv.');
+    fs.writeFileSync('output_programs.csv', "\uFEFF" + csvContent, 'utf8');
+    console.log('Exported as output_programs.csv.');
 }
 
+// Main code to call funtions
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -172,4 +274,5 @@ function exportToCSV(programs) {
     await browser.close();
 
     exportToCSV(programs);
+
 })();
